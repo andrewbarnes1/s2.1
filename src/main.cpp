846c3efa95600d474416b243ce15adfbd9bb0059
andrew <andrewbarneswork@outlook.com>
2018-01-25 08:46:00 +0000

    Added all joystick motions with rise and fall.

--- src/main.cpp
+++ src/main.cpp
@@ -5,34 +5,41 @@ DigitalOut green(LED_GREEN);
 
 void on(void)
 {
-    red.write(0);
-    // Puts writes to the standard ouput and appends a new line
-    puts("rising 0->1");
+  red.write(0);
+  // Puts = write to the standard ouput and appends a new line
+  puts("rising 0->1");
 }
 void off(void)
 {
-    red.write(1);
-    puts("falling 1->0");
+  red.write(1);
+  puts("falling 1->0");
 }
 
 int main() {
-  // Switches on the board are the other way round.
-    InterruptIn  left(SW2);
-    InterruptIn right(SW3);
 
-    // Joystick interrupts
-    InterruptIn up(A2);
-    InterruptIn down(A3);
+  // Switches on the bottom board.
+  InterruptIn  left(SW2);
+  InterruptIn right(SW3);
 
-    up.rise(on);
-    up.fall(off);
+  // Joystick interrupts
+  InterruptIn up(A2);
+  InterruptIn down(A3);
+  InterruptIn joyLeft(A4);
+  InterruptIn joyRight(A5);
 
-    down.rise(on);
-    down.fall(off);
+  up.rise(on);
+  up.fall(off);
+  down.rise(on);
+  down.fall(off);
+  joyLeft.rise(on);
+  joyLeft.fall(off);
+  joyRight.rise(on);
+  joyRight.fall(off);
 
-    left.rise(on);
-    right.fall(off);
-    while(1) {
-        /* GNDN */
-    }
+  // Switches on the bottom board
+  left.rise(on);
+  right.fall(off);
+  while(1) {
+    /* GNDN */
+  }
 }
